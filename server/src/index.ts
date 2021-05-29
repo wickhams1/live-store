13c9e4e2db9f81f4dea7c7c1b2c9c5ca605002e0
wickhams1 <sam.w@linktr.ee>
2021-05-29 16:40:49 +1000

    task: convert to typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-const express = require("express");
+import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
