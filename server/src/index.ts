c0ec24852a1f4b9ade7c76c44ad17b6231c1e10c
wickhams1 <sam.w@linktr.ee>
2021-05-30 16:12:06 +1000

    feat: graphql resolver using class

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,39 @@ import express, { Request, Response } from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
 
+class Person {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: Person) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greeting() {
+    return `Hello from ${this.firstName} ${this.lastName}`;
+  }
+}
+
 const schema = buildSchema(`
+  type Person {
+    firstName: String!
+    lastName: String!
+    greeting: String!
+  }
+
   type Query {
     hello: String
+    person(firstName: String!, lastName: String!): Person
   }
 `);
 
 const root = {
   hello: () => {
     return 'Hello World!';
+  },
+  person: (person: Person) => {
+    return new Person(person);
   },
 };
 
