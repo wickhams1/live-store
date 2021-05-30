d539a38571fef84b355192ede1bfbdd7b23f5f0d
wickhams1 <sam.w@linktr.ee>
2021-05-30 15:47:55 +1000

    feat: added graphql in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,30 @@
 import express, { Request, Response } from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello World!';
+  },
+};
+
 const app = express();
 const port = 3000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
