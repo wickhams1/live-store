8922b20ff39c3e110650caaa3ae14a1a1b715254
wickhams1 <sam.w@linktr.ee>
2021-06-27 16:17:36 +1000

    feat: login form components

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+import { LoginFormWrapper, LoginFormInputWrapper, LoginFormButtonWrapper } from './styles';
+import { Button, Input } from '../';
+
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [emailAddress, setEmailAddress] = useState('');
+
+  return (
+    <LoginFormWrapper>
+      <LoginFormInputWrapper>
+        <Input
+          id="emailAddress"
+          label="Email Address"
+          placeholder="Email Address"
+          value={emailAddress}
+          onChange={(e) => setEmailAddress(e.target.value)}
+        />
+      </LoginFormInputWrapper>
+      <LoginFormButtonWrapper>
+        <Button>Login</Button>
+      </LoginFormButtonWrapper>
+    </LoginFormWrapper>
+  );
 };
 
 export default LoginForm;
---
+++ client/src/components/LoginForm/styles.ts
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+import { Theme } from '../../theme';
+
+export const LoginFormWrapper = styled.div`
+  ${({ theme: { space } }: { theme: Theme }) => css`
+    width: 100%;
+    padding: ${space(4)} ${space(2)};
+    box-sizing: border-box;
+    border: 1px dashed black;
+    border-radius: ${space(2)};
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  `}
+`;
+
+export const LoginFormInputWrapper = styled.div`
+  ${({ theme: { space } }: { theme: Theme }) => css`
+    width: 100%;
+    box-sizing: border-box;
+    margin: ${space(3)} 0;
+  `}
+`;
+
+export const LoginFormButtonWrapper = styled.div`
+  ${({ theme: { space } }: { theme: Theme }) => css`
+    width: 100%;
+    box-sizing: border-box;
+    height: ${space(8)};
+    margin: ${space(3)} 0;
+  `}
+`;
